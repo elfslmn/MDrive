f76177f63016283971e8382d25838a33c541508e
Elif Salman <elifsalman94@hotmail.com>
2018-10-09 17:32:19 +0300

    arrange master runnable

--- src/main/java/DriveQuickstart.java
+++ src/main/java/DriveQuickstart.java
@@ -1,21 +1,14 @@
-import com.google.api.services.drive.model.File;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.security.GeneralSecurityException;
-import java.time.LocalDateTime;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 // code from https://developers.google.com/drive/api/v3/quickstart/java
@@ -28,11 +21,12 @@ public class DriveQuickstart {
     private static final java.io.File DESKTOP_FOLDER = new java.io.File(System.getProperty("user.home"), "Desktop");
     public static java.io.File LOCAL_DRIVE_FOLDER;
 
-    private static final ScheduledExecutorService driveSyncer = Executors.newScheduledThreadPool(1);
-    private static long lastSync;
+    private static final ScheduledExecutorService driveSynchroniser = Executors.newScheduledThreadPool(1);
+    public static long lastSync;
 
     private static Scanner input = new Scanner(System.in);
     private static Mode mode = null;
+    public static final HashMap<String, SyncedFile> syncedFiles = new HashMap<String, SyncedFile>(); //TODO may use syncronized Map
 
     public static void main(String... args) throws IOException, GeneralSecurityException {
 
@@ -42,7 +36,7 @@ public class DriveQuickstart {
             if(i == 1){
                 System.out.println("Mode : MASTER");
                 mode = Mode.MASTER;
-                LOCAL_DRIVE_FOLDER = new java.io.File(DESKTOP_FOLDER, "DriveCloudMaster");
+                LOCAL_DRIVE_FOLDER = new java.io.File(DESKTOP_FOLDER, "MasterDriveCloud");
                 try {
                     FileReader fr = new FileReader("master_data.txt");
                     BufferedReader br = new BufferedReader(fr);
@@ -55,9 +49,19 @@ public class DriveQuickstart {
                 break;
             }
             else if(i == 0){
-                mode = Mode.FOLLOWER;
-                LOCAL_DRIVE_FOLDER = new java.io.File(DESKTOP_FOLDER, "DriveCloudFollower");
                 System.out.println("Mode : FOLLOWER");
+                mode = Mode.FOLLOWER;
+                // TODO handle for multiple followers
+                LOCAL_DRIVE_FOLDER = new java.io.File(DESKTOP_FOLDER, "FollowerDriveCloud");
+                try {
+                    FileReader fr = new FileReader("follower_data.txt");
+                    BufferedReader br = new BufferedReader(fr);
+                    String s = br.readLine();
+                    lastSync = Long.parseLong(s);
+                }catch (IOException ex){
+                    lastSync = -1;
+                }
+
                 break;
             }
             System.out.println("Invalid input");
@@ -77,91 +81,21 @@ public class DriveQuickstart {
             System.out.println("Created Sync Folder: " + LOCAL_DRIVE_FOLDER.getAbsolutePath());
         }
 
-        final HashMap<String, SyncedFile> syncedFiles = new HashMap<String, SyncedFile>(); //TODO may use syncronized Map
-
-        driveSyncer.scheduleWithFixedDelay(new Runnable() {
-            @Override
-            public void run() {
-                HashMap<String, File> cloudFiles = GoogleDriveUtils.getAppDataFileMap();
-                java.io.File[] localFiles = LOCAL_DRIVE_FOLDER.listFiles();
-
-                LinkedList<java.io.File> needUpload = new LinkedList<>();
-                LinkedList<File> needDelete = new LinkedList<>();
-                LinkedList<java.io.File> needUpdate = new LinkedList<>();
-                syncedFiles.clear();
-
-                for(java.io.File localFile : localFiles){
-                    // The file is only in LOCAL folder, send it to cloud
-                    if(!cloudFiles.containsKey(localFile.getName())){
-                        needUpload.add(localFile);
-                    }else{
-                        // The file in the local is updated since last sync
-                        if(localFile.lastModified() > lastSync){
-                            needUpdate.add(localFile);
-                            syncedFiles.put(localFile.getName(), null);
-                        }else {
-                            File cloudFile = cloudFiles.get(localFile.getName());
-                            SyncedFile sf = new SyncedFile(localFile, cloudFile);
-                            syncedFiles.put(localFile.getName(), sf);
-                        }
-
-                    }
-                }
-
-                for(String cloudFile: cloudFiles.keySet()){
-                    // The file is only in CLOUD folder, delete from local
-                    if(!syncedFiles.containsKey(cloudFile)){
-                        needDelete.add(cloudFiles.get(cloudFile));
-                    }
-                }
-
-                if(needDelete.size() == 0 && needUpload.size()==0 && needUpdate.size()==0){
-                    System.out.println("Current time: " + LocalDateTime.now() + ",\tno update is needed. Already synced!");
-                }else{
-                    // Print planning updates
-                    System.out.println("Current time: " + LocalDateTime.now() + ",\tthe following files are going to be synchronized");
-                    for(java.io.File f : needUpload){
-                        System.out.println(f.getName()+" \tgoing to be uploaded to cloud\t Size= " + f.length()+" bytes");
-                    }
-                    for(File f : needDelete){
-                        System.out.println(f.getName()+" \tgoing to be deleted from cloud\t Size= " + f.getSize()  +" bytes");
-                    }
-                    for(java.io.File f : needUpdate){
-                        System.out.println(f.getName()+" \tgoing to be updated in cloud\t Size= " + f.length()  +" bytes");
-                    }
-
-                    // Apply planning updates
-                    for(java.io.File localFile : needUpload){
-                        File cloudFile = GoogleDriveUtils.createFileInAppData(localFile);
-                        SyncedFile sf = new SyncedFile(localFile, cloudFile);
-                        syncedFiles.put(localFile.getName(), sf);
-                    }
-                    for(File cloudFile : needDelete){
-                        GoogleDriveUtils.deleteFile(cloudFile);
-                    }
-                    for(java.io.File localFile : needUpdate){
-                        File cloudFile = cloudFiles.get(localFile.getName());
-                        GoogleDriveUtils.updateFile(cloudFile.getId(), localFile);
-                        SyncedFile sf = new SyncedFile(localFile, cloudFile);
-                        syncedFiles.put(localFile.getName(), sf);
-
-                    }
-                    System.out.println("Current time: "+LocalDateTime.now()+ ",\tSynchronization done with Google Drive");
-                }
-                lastSync = System.currentTimeMillis();
-            }
-        }, 0, 30, TimeUnit.SECONDS);
-
+        if(mode == Mode.MASTER) {
+            driveSynchroniser.scheduleWithFixedDelay(new MasterCloudSynchronizer(), 0, 30, TimeUnit.SECONDS);
+        }
 
         while(true){
             String s = input.nextLine();
             if(s.equals("quit")){
                 System.out.println("Program will be closed");
-                driveSyncer.shutdownNow();
+                if(mode == Mode.MASTER) driveSynchroniser.shutdownNow();
                 break;
             }
         }
         input.close();
+
+        // Save the last sync time for further use of project
         FileWriter fw;
         if(mode == Mode.MASTER) {
             fw = new FileWriter("master_data.txt");
@@ -178,3 +112,6 @@ public class DriveQuickstart {
         return;
     }
 }
+
+
+
---
+++ src/main/java/MasterCloudSynchronizer.java
@@ -0,0 +1,82 @@
+import com.google.api.services.drive.model.File;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * Created by esalman17 on 9.10.2018.
+ */
+
+public class MasterCloudSynchronizer implements Runnable {
+    @Override
+    public void run() {
+        HashMap<String, File> cloudFiles = GoogleDriveUtils.getAppDataFileMap();
+        java.io.File[] localFiles = DriveQuickstart.LOCAL_DRIVE_FOLDER.listFiles();
+
+        LinkedList<java.io.File> needUpload = new LinkedList<>();
+        LinkedList<File> needDelete = new LinkedList<>();
+        LinkedList<java.io.File> needUpdate = new LinkedList<>();
+        DriveQuickstart.syncedFiles.clear();
+
+        for (java.io.File localFile : localFiles) {
+            // The file is only in LOCAL folder, send it to cloud
+            if (!cloudFiles.containsKey(localFile.getName())) {
+                needUpload.add(localFile);
+            } else {
+                // The file in the local is updated since last sync
+                if (localFile.lastModified() > DriveQuickstart.lastSync) {
+                    needUpdate.add(localFile);
+                    DriveQuickstart.syncedFiles.put(localFile.getName(), null);
+                } else {
+                    File cloudFile = cloudFiles.get(localFile.getName());
+                    SyncedFile sf = new SyncedFile(localFile, cloudFile);
+                    DriveQuickstart.syncedFiles.put(localFile.getName(), sf);
+                }
+
+            }
+        }
+
+        for (String cloudFile : cloudFiles.keySet()) {
+            // The file is only in CLOUD folder, delete from local
+            if (!DriveQuickstart.syncedFiles.containsKey(cloudFile)) {
+                needDelete.add(cloudFiles.get(cloudFile));
+            }
+        }
+
+        if (needDelete.size() == 0 && needUpload.size() == 0 && needUpdate.size() == 0) {
+            System.out.println("Current time: " + LocalDateTime.now() + ",\tno update is needed. Already synced!");
+        } else {
+            // Print planning updates
+            System.out.println("Current time: " + LocalDateTime.now() + ",\tthe following files are going to be synchronized");
+            for (java.io.File f : needUpload) {
+                System.out.println(f.getName() + " \tgoing to be uploaded to cloud\t Size= " + f.length() + " bytes");
+            }
+            for (File f : needDelete) {
+                System.out.println(f.getName() + " \tgoing to be deleted from cloud\t Size= " + f.getSize() + " bytes");
+            }
+            for (java.io.File f : needUpdate) {
+                System.out.println(f.getName() + " \tgoing to be updated in cloud\t Size= " + f.length() + " bytes");
+            }
+
+            // Apply planning updates
+            for (java.io.File localFile : needUpload) {
+                File cloudFile = GoogleDriveUtils.createFileInAppData(localFile);
+                SyncedFile sf = new SyncedFile(localFile, cloudFile);
+                DriveQuickstart.syncedFiles.put(localFile.getName(), sf);
+            }
+            for (File cloudFile : needDelete) {
+                GoogleDriveUtils.deleteFile(cloudFile);
+            }
+            for (java.io.File localFile : needUpdate) {
+                File cloudFile = cloudFiles.get(localFile.getName());
+                GoogleDriveUtils.updateFile(cloudFile.getId(), localFile);
+                SyncedFile sf = new SyncedFile(localFile, cloudFile);
+                DriveQuickstart.syncedFiles.put(localFile.getName(), sf);
+
+            }
+            System.out.println("Current time: " + LocalDateTime.now() + ",\tSynchronization done with Google Drive");
+        }
+        DriveQuickstart.lastSync = System.currentTimeMillis();
+    }
+}
